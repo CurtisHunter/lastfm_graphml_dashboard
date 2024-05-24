c58b5e6e2986478b6ad27235b82463ba5c0d3be1
curtiscoding1 <curtis.p.hunter@gmail.com>
2024-05-24 15:56:23 +0100

    removing more unnecessary packages

--- LastFM_App.py
+++ LastFM_App.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from sklearn.model_selection import train_test_split
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import random, os
