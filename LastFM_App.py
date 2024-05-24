c4aa309962a647f54b79471052124a37764321c2
curtiscoding1 <curtis.p.hunter@gmail.com>
2024-05-24 15:49:01 +0100

    removing unncessary packages

--- LastFM_App.py
+++ LastFM_App.py
@@ -8,9 +8,6 @@ import pandas as pd
 import json
 import networkx as nx
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
 from networkx.algorithms import community
 import matplotlib.pyplot as plt
 import requests
